468b71d0237c784932dd3eb9105e6a00f907260b
Shailesh Gharti <124006792+Men-ace@users.noreply.github.com>
2025-05-06 16:22:59 +1000

    updated resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
           <br />
           <div className="flex justify-end">
             <button className="btn">{hero.btnText}</button>
-           <a target="blank" href="/src/assets/images/Hero/Resume.pdf"><button className="btn-cv">{hero.btnCV}</button></a> 
+           <a target="blank" href="public\Shailesh Gharti (1).pdf"><button className="btn-cv">{hero.btnCV}</button></a> 
           </div>
           <div className="flex flex-col gap-10 mt-10">
             {hero.hero_content.map((content, i) => (
